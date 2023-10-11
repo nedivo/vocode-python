292868aa0c44706cfaac0a5b639675e23dc817be
Mike Biancalana <michael@nedivo.co>
2023-10-11 12:27:58 -0400

    Add bypass transfer TwiML template

--- vocode/streaming/telephony/templater.py
+++ vocode/streaming/telephony/templater.py
@@ -1,12 +1,13 @@
-import os
-from jinja2 import Environment, FileSystemLoader
+from pathlib import Path
+
 from fastapi import Response
+from jinja2 import Environment, FileSystemLoader
 
 
 class Templater:
     def __init__(self):
         self.templates = Environment(
-            loader=FileSystemLoader("%s/templates/" % os.path.dirname(__file__))
+            loader=FileSystemLoader(Path(__file__).parent / "templates/")
         )
 
     def render_template(self, template_name: str, **kwargs):
@@ -16,5 +17,11 @@ class Templater:
     def get_connection_twiml(self, call_id: str, base_url: str):
         return Response(
             self.render_template("connect_call.xml", base_url=base_url, id=call_id),
+            media_type="application/xml",
+        )
+
+    def get_bypass_twiml(self, transfer_to: str):
+        return Response(
+            self.render_template("bypass_transfer.xml", transfer_to=transfer_to),
             media_type="application/xml",
         )
